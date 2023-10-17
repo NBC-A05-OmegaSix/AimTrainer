4ca5e48aba98db27d7521ed3d79a76679f019397
dohyeon9483 <dh4934@naver.com>
2023-10-17 17:12:28 +0900

    [ADD] Current Gun Image UI / Gurrent Shooting State UI (Single, Brust, FullAuto)

--- Assets/Scripts/UI/Menu/AmmosUI.cs
+++ Assets/Scripts/UI/Menu/AmmosUI.cs
@@ -5,18 +5,52 @@ public class AmmosUI : MonoBehaviour
 {
     public TextMeshProUGUI currentAmmoText;
     public TextMeshProUGUI totalAmmoText;
+    public TextMeshProUGUI shootingState;
+    public GameObject m16ImagePrefab; 
+    public GameObject ak47ImagePrefab;
+    public GameObject trgImagePrefab;
     private Gun gun; 
 
-    // Start is called before the first frame update
     void Start()
     {
         gun = FindObjectOfType<Gun>(); 
         totalAmmoText.text = gun.maxAmmo.ToString(); // 총알 최대 수를 나타내는 값 설정 (총의 종류에 따라 수정될 예정)
     }
 
-    // Update is called once per frame
     void Update()
     {
-        currentAmmoText.text = gun.currentAmmo.ToString(); // 현재 총알 수
+        ShowGunImage();
+
+        currentAmmoText.text = gun.currentAmmo.ToString();
+
+        if (gun.currentFireMode == Gun.FireMode.Single)
+        {
+            shootingState.text = "Single"; 
+        }
+        else if (gun.currentFireMode == Gun.FireMode.Burst3)
+        {
+            shootingState.text = "Burst"; 
+        }
+        else if (gun.currentFireMode == Gun.FireMode.FullAuto)
+        {
+            shootingState.text = "Full Auto"; 
+        }
     }
+
+    private void ShowGunImage()
+    {
+        //    if (gun.gunType == Gun.GunType.AK47) // gunType에 따라 이미지 변경
+        //    {
+        //        // AK47 이미지 표시 코드
+        //    }
+        //    else if (gun.gunType == Gun.GunType.M16)
+        //    {
+        //        // M16 이미지 표시 코드
+        //    }
+        //    else if (gun.gunType == Gun.GunType.TRG)
+        //    {
+        //        // TRG 이미지 표시 코드
+        //    }
+    }
+
 }
