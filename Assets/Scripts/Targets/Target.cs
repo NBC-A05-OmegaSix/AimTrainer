84c7135aa931c9febc7efc6f2f06e872787f3647
GYALLERHORN <kjins1998@gmail.com>
2023-10-18 22:11:05 +0900

    [UDATE] TARGET MOVING LOGIC

--- Assets/Scripts/Targets/Target.cs
+++ Assets/Scripts/Targets/Target.cs
@@ -12,16 +12,25 @@ public class Target : MonoBehaviour
     private int currentHealth;
     public float respawnTime = 2.0f; //타겟 재생성 시간
     private Coroutine respawnCoroutine;
+    [SerializeField]
+    private bool isMovable;
 
- 
+    [Header("Moving Target")]
+    public float moveSpeed = 4.0f; //움직임 속도
+    public float moveDistance = 10.0f; //좌우로 움직일 거리
+    private Vector3 initialPosition;
+    private bool movingRight = true;
+
+
     private void Awake()
     {
         audiosource = GetComponent<AudioSource>();
     }
 
-
     private void Start()
     {
+        initialPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
         currentHealth = maxHealth;
     }
     public void TakeDamage(int damage)
@@ -32,8 +41,34 @@ public class Target : MonoBehaviour
         {
             //타겟 비활성화
             gameObject.SetActive(false);
-
         }
     }
+    void Update()
+    {
+        if (isMovable)
+        {
+            MoveTarget();
+        }
 
+        Debug.Log(Vector3.Distance(initialPosition, transform.position).ToString());
+    }
+
+    private void MoveTarget()
+    {
+        //좌우로 움직이는 로직
+        if (movingRight)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        //일정 거리를 이동하면 방향을 바꿈
+        if (Vector3.Distance(initialPosition, transform.position) > moveDistance)
+        {
+            if (movingRight) movingRight = false;
+            else movingRight = true;
+        }
+    }
 }
