dae62bd73f8cbb125a1cffe3fa47eee6c01e6d27
dohyeon9483 <dh4934@naver.com>
2023-10-19 11:36:16 +0900

    [UPDATE] Session / AmmoUI

--- Assets/Scripts/UI/Menu/AmmosUI.cs
+++ Assets/Scripts/UI/Menu/AmmosUI.cs
@@ -1,56 +1,22 @@
 using UnityEngine;
 using TMPro;
 
-public class AmmosUI : MonoBehaviour
+public class AmmoUI : MonoBehaviour
 {
+    public Gun gun; 
     public TextMeshProUGUI currentAmmoText;
-    public TextMeshProUGUI totalAmmoText;
-    public TextMeshProUGUI shootingState;
-    public GameObject m16ImagePrefab; 
-    public GameObject ak47ImagePrefab;
-    public GameObject trgImagePrefab;
-    private Gun gun; 
+    public TextMeshProUGUI maxAmmoText;
+    public TextMeshProUGUI shootingStateText;
 
     void Start()
     {
-        gun = FindObjectOfType<Gun>(); 
-        totalAmmoText.text = gun.maxAmmo.ToString(); // 총알 최대 수를 나타내는 값 설정 (총의 종류에 따라 수정될 예정)
     }
 
     void Update()
     {
-        ShowGunImage();
+            currentAmmoText.text = gun.currentAmmo.ToString();
+            maxAmmoText.text = gun.maxAmmo.ToString();
+        shootingStateText.text = gun.currentFireMode.ToString();
 
-        currentAmmoText.text = gun.currentAmmo.ToString();
-
-        if (gun.currentFireMode == Gun.FireMode.Single)
-        {
-            shootingState.text = "Single"; 
-        }
-        else if (gun.currentFireMode == Gun.FireMode.Burst3)
-        {
-            shootingState.text = "Burst"; 
-        }
-        else if (gun.currentFireMode == Gun.FireMode.FullAuto)
-        {
-            shootingState.text = "Full Auto"; 
-        }
     }
-
-    private void ShowGunImage()
-    {
-        //    if (gun.gunType == Gun.GunType.AK47) // gunType에 따라 이미지 변경
-        //    {
-        //        // AK47 이미지 표시 코드
-        //    }
-        //    else if (gun.gunType == Gun.GunType.M16)
-        //    {
-        //        // M16 이미지 표시 코드
-        //    }
-        //    else if (gun.gunType == Gun.GunType.TRG)
-        //    {
-        //        // TRG 이미지 표시 코드
-        //    }
-    }
-
 }
